44ad3a70bbacf36f8bc94880a50c4e7c6cb39218
Douglas <douglasnavarro94@gmail.com>
2018-08-27 09:35:34 -0300

    group by part 1

--- SQL_for_data_analysis/lesson_3_sql_aggregations/group_by.sql
+++ SQL_for_data_analysis/lesson_3_sql_aggregations/group_by.sql
@@ -14,20 +14,45 @@ JOIN accounts a
 ON a.id = o.account_id
 GROUP BY a.name;
 
--- Via what channel did the most recent (latest) web_event occur, which account was associated with this web_event? Your query should return only three values - the date, channel, and account name.
-
+-- Via what channel did the most recent (latest) web_event occur, which account was associated with this web_event? 
+-- Your query should return only three values - the date, channel, and account name.
+SELECT web_events.channel, web_events.occurred_at, accounts.name
+FROM web_events
+JOIN accounts
+ON web_event.account_id = accounts.id
+ORDER BY web_events.occurred_at DESC
+LIMIT 1;
 
 -- Find the total number of times each type of channel from the web_events was used.
 -- Your final table should have two columns - the channel and the number of times the channel was used.
-
+SELECT web_events.channel, COUNT(*) AS times_used
+FROM web_events
+GROUP BY web_events.channel
 
 -- Who was the primary contact associated with the earliest web_event?
+SELECT a.primary_poc
+FROM web_events w
+JOIN accounts a
+ON a.id = w.account_id
+ORDER BY w.occurred_at
+LIMIT 1;
 
 -- What was the smallest order placed by each account in terms of total usd.
 -- Provide only two columns - the account name and the total usd.
 -- Order from smallest dollar amounts to largest.
+SELECT a.primary_poc
+FROM web_events w
+JOIN accounts a
+ON a.id = w.account_id
+ORDER BY w.occurred_at
+LIMIT 1;
 
 -- Find the number of sales reps in each region.
 -- Your final table should have two columns - the region and the number of sales_reps.
 -- Order from fewest reps to most reps.
-
+SELECT region.name, COUNT(*) sales_reps_count
+FROM region
+JOIN sales_reps
+ON sales_reps.region_id = region.id
+GROUP BY region.name
+ORDER BY sales_reps_count;
